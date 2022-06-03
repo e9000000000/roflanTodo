e21d1d5e2ab82dedb6dce032411764ce051ce971
6 <skipper224483@gmail.com>
2022-06-03 23:23:07 +0300

    add simple menu

--- Main.hs
+++ Main.hs
@@ -1,5 +1,19 @@
 module Main where
 
+import System.Exit (exitSuccess)
+
+data Tasks = Tasks [String]
+
+
+processCommand :: Integer -> IO ()
+processCommand 0 = exitSuccess
+processCommand _ = putStrLn "notheing"
+
+getInt :: IO Integer
+getInt = readLn
+
 main :: IO ()
 main = do
-	putStrLn "ffeojwofewoefjoife"
+	inp <- getInt
+	processCommand inp
+	main
