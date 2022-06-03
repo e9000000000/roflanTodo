3890f7e5381f59edc95a4e4e20ac1b917f9bc321
6 <skipper224483@gmail.com>
2022-06-04 00:52:13 +0300

    base working

--- Main.hs
+++ Main.hs
@@ -2,18 +2,66 @@ module Main where
 
 import System.Exit (exitSuccess)
 
-data Tasks = Tasks [String]
+data Task = Task { text :: String
+								 , isCompleted :: Bool
+								 } deriving (Show)
 
+type Tasks = [Task]
 
-processCommand :: Integer -> IO ()
-processCommand 0 = exitSuccess
-processCommand _ = putStrLn "notheing"
+data Result = Result { result :: String
+										 , tasks :: Tasks
+										 } deriving (Show)
 
-getInt :: IO Integer
-getInt = readLn
+addTask :: Tasks -> String -> Result
+addTask tasks text = Result "success" ((Task text False):tasks)
+
+deleteTask :: Tasks -> Int -> Result
+deleteTask tasks n
+	| n >= 0 && n < length tasks = Result "success" newTasks
+	| otherwise = Result "wrong task number" tasks
+		where
+			left = take n tasks
+			right = drop (n+1) tasks
+			newTasks = left ++ right
+
+completeTask :: Tasks -> Int -> Result
+completeTask tasks n
+	| n >= 0 && n < length tasks = Result "success" newTasks
+	| otherwise = Result "wrong task number" tasks
+		where
+			left = take n tasks
+			right = drop (n+1) tasks
+			task = head $ take (n+1) tasks
+			newTasks = left ++ (Task (text task) True):right
+
+processCommand :: Tasks -> String -> Result
+processCommand tasks cmd
+	| n == 0 = Result "exit" tasks
+	| n == 1 = Result (show tasks) tasks
+	| n == 2 = addTask tasks inp
+	| n == 3 = deleteTask tasks (read inp)
+	| n == 4 = completeTask tasks (read inp)
+	| otherwise = Result "wrong option" tasks
+	where
+		args = words cmd
+		n = read $ head args
+		inp = unwords $ tail args
+
+processCommands :: Tasks -> IO ()
+processCommands ts = do
+	putStrLn "SHASHLIKI TODO LIST"
+	putStrLn "\t0 - save and exit"
+	putStrLn "\t1 - task list"
+	putStrLn "\t2 TEXT - add task"
+	putStrLn "\t3 N - delete task"
+	putStrLn "\t4 N - complete task"
+	cmd <- getLine
+	let r = processCommand ts cmd
+	if result r == "exit" then return () else do
+		putStrLn $ result r
+		putStrLn ""
+		processCommands $ tasks r
 
 main :: IO ()
 main = do
-	inp <- getInt
-	processCommand inp
-	main
+	processCommands []
